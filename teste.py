780ca2a1afd25bb6eca04dfd4e2601d0152c24ae
KaiqueAndeloci <67813976+KaiqueAndeloci@users.noreply.github.com>
2021-06-22 23:26:18 -0300

    .

--- teste.py
+++ teste.py
@@ -1,3 +1,4 @@
+import os
 import tkinter as tk
 
 #from tkinter import *
@@ -9,16 +10,28 @@ class Login ():
     def __init__(self, tela = tk.Tk()) -> None:
         #config tela
         tela.title("Login")
-        tela.iconbitmap("A:\Pasta-kaique\Gitihub\KaiqueAndeloci\projetoRPG\Imagens\login.ico")
+        tela.iconbitmap(caminho)
 
         #tamanho da tela
         #tela.geometry("300x300")
         tela.minsize(300, 300)
 
         #Declaração da Label
-
         #Declaração do botão
 
         tela.mainloop()
 
+a = os.listdir(".")
+for i in a:
+    if i == "Imagens":
+        caminho = os.path.abspath(i) + "\login.ico"
+
 Login()
+
+"""
+import os
+for caminho, diretorios, arquivos in os.walk(''):
+    print(caminho)
+    print(diretorios)
+    print(arquivos)
+"""
