5d84da7feea68cc5c124c575d34bc69194936698
KaiqueAndeloci <67813976+KaiqueAndeloci@users.noreply.github.com>
2021-07-03 13:54:35 -0300

    .

--- json_manager.py
+++ json_manager.py
@@ -1,9 +1,37 @@
+from os import path
 from os.path import dirname, realpath, isfile
-from json import dump
+from json import dump, load
+
 
 class JsonManager():
     def __init__(self):
         self.path = dirname(realpath(__file__))# + "/"
+        print(path)
     
-    def create_json():
-        data = {}
+    def create_json(self, file, dicionario):
+        data = dicionario
+        path_data_json = self.path  + "/" + file
+
+        if not isfile(path_data_json):
+            with open(path_data_json, 'w') as f:
+                dump(data, f, indent = 4, separators = (',', ':'))
+            return True
+        else:
+            return False
+
+    def update(self, file, dicionario):
+        pass
+    def read(self, file):
+        if isfile(self.path + file):
+            with open(self.path + file) as f:
+                data = load(f)
+            return data
+        else:
+            return False
+
+if __name__ == '__main__':
+    jmaneger = JsonManager()
+    dic = {
+        
+    }
+    print(jmaneger.create_json("test.json", dic))
