d33508993f91e7eb404cfa254417e7a89de3cd1b
KaiqueAndeloci <67813976+KaiqueAndeloci@users.noreply.github.com>
2021-09-06 12:45:37 -0300

    ASDFASF
    FASDFDS

--- conectando_cliente.py
+++ conectando_cliente.py
@@ -1,4 +1,15 @@
 import socket
 
-
+ip_user = socket.gethostbyname(socket.gethostname())
+print(ip_user)
 cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+loop = True
+while loop:
+    mensagem = input("Digite: ")
+    if mensagem != "/sair":
+        cliente.sendto(mensagem.encode(), (ip_user, 12000))
+        print("Enviado")
+        mensagem_sever, endereco_sever = cliente.recvfrom(2048)
+        print(f"O sever dissse: {mensagem_sever.decode()}")
+    else:
+        loop = False
--- conectando_servidor.py
+++ conectando_servidor.py
@@ -1,13 +1,15 @@
 import socket
 
 servidor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ip_user = socket.gethostbyname(socket.gethostname())
 
-servidor.bind(('localhost', 12000))
+servidor.bind((ip_user, 12000))
 
 while True:
+    print("Escutando...")
     menssagem_bytes, enderc_ip_cliente = servidor.recvfrom(2048)
     print(enderc_ip_cliente)
     print(f"O cliente escreveu: {menssagem_bytes.decode()}.")
     resposta = input("Escreva uma mensagem: ")
     servidor.sendto(resposta.encode(), enderc_ip_cliente)
-    print("pronto")
+    print("Enviado")
